2ee1a87ba587dcd01ab24c65f697d5f026b5c1dd
nickjevershed <nick.evershed@gmail.com>
2016-03-04 15:41:49 +1100

    committtt

--- app/analyse.py
+++ app/analyse.py
@@ -1,53 +1,67 @@
 from datetime import datetime
 import pandas as pd
+import simplejson as json
 
 #Analyse all the things
 
 def niceNumber(n):
+
 	if n < 1000000:
-		return format(n, ",d")
+		return format(int(n), ",d")
 	if n > 999999 and n < 1000000000:
-		return format(n, ",d") + 'm'
+		return format(int(n)/1000000, ",d") + 'm'
 	if n > 999999999 and n < 1000000000000:
-		return format(n, ",d") + 'bn'	
+		return format(int(n)/1000000000, ",d") + 'bn'	
 
 def analyseData(url,source,units,entity):
 	results = {}
-	results['summary'] = {}
+	results["summary"] = {}
 
 	df = pd.read_csv(url)
-	results['allresults'] = df.sort_values(by='value',ascending=False).to_json(orient='records')
-	
+
 	# print df
 	#Year trend
 	gpYear = df.groupby('datetime')
 	yearSum = gpYear.sum()
 	yearMean = gpYear.mean()
 	# print yearSum.to_json()
-	results['summary']['yearSum'] = yearSum.to_json()
-	results['summary']['yearMean'] = yearMean.to_json()
+	results["summary"]["yearSum"] = yearSum.to_json()
+	results["summary"]["yearMean"] = yearMean.to_json()
+	results["all"] = df.sort_values(by='value',ascending=False).to_json(orient='records')
 
-	# print yearSum.sort(ascending=False)['value'].iloc[0]
-	mostRecentYearTotal = yearSum.sort_index(ascending=False)['value'].iloc[0]
-	results['summary']['mostRecentYearTotal'] = mostRecentYearTotal
+	yearPctChange = yearSum.pct_change()
+
+	currYearPctChange = round(yearPctChange.sort_index(ascending=False)["value"].iloc[0] * 100,1)
+	results["summary"]["currYearPctChange"] = currYearPctChange
+
+	print yearSum.sort(ascending=False)["value"].iloc[0]
+	mostRecentYearTotal = yearSum.sort_index(ascending=False)["value"].iloc[0]
+	results["summary"]["mostRecentYearTotal"] = mostRecentYearTotal
+	results["summary"]["mostRecentYearTotalStr"] = niceNumber(mostRecentYearTotal)
 
 	mostRecentYear = yearSum.sort_index(ascending=False).index[0]
-	results['summary']['mostRecentYear'] = mostRecentYear
+	results["summary"]["mostRecentYear"] = mostRecentYear
 
-	mostRecentYearMean = yearMean.sort_index(ascending=False)['value'].iloc[0]
-	results['summary']['mostRecentYearMean'] = mostRecentYearMean
+	mostRecentYearMean = yearMean.sort_index(ascending=False)["value"].iloc[0]
+	results["summary"]["mostRecentYearMean"] = mostRecentYearMean
 	
 	# print mostRecentYear
-	biggestEntity = df.sort_values(by='value',ascending=False)['name'].iloc[0]
+	biggestEntity = df.sort_values(by="value",ascending=False)["name"].iloc[0]
 	
-	results['summary']['biggestEntity'] = biggestEntity
+	results["summary"]["biggestEntity"] = biggestEntity
 
-	dfnonull = df[(df['value'] != 0) & pd.notnull(df['value'])]
-	# dfnonull = dfnozeros[pd.notnull(df['value'])]
+	dfnonull = df[(df["value"] != 0) & pd.notnull(df["value"])]
+	# dfnonull = dfnozeros[pd.notnull(df["value"])]
 
-	smallestEntity = dfnonull.sort_values(by='value',ascending=True)['name'].iloc[0]
-	results['summary']['smallestEntity'] = smallestEntity
+	smallestEntity = dfnonull.sort_values(by="value",ascending=True)["name"].iloc[0]
+	results["summary"]["smallestEntity"] = smallestEntity
 
+	topTen = []
+	for x in xrange(0,10):
+		topTen.append(df[df['datetime'] == mostRecentYear].sort_values(by="value",ascending=False)["name"].iloc[x])
+			
+	results["summary"]["topTen"] = topTen
+		
 	onlynull = df[(df['datetime'] == mostRecentYear) & pd.isnull(df['value'])]
 	if len(onlynull.index) > 0:
 		if len(onlynull.index) == 1:
@@ -56,8 +70,16 @@ def analyseData(url,source,units,entity):
 			nullSentence = "There were " + str(onlynull.index) + " " + entity + " that did not report in " + mostRecentYear 
 		results['summary']['nullSentence'] = nullSentence
 
-	print results
-	return results	
-analyseData('https://docs.google.com/spreadsheets/d/1l49PR88epvzcXGDReLJ-xa2DbtQmRLQN6g-SoqGgSaM/pub?output=csv','Clean Energy Regulator','tonnes of CO2 equivlaent','corporation')	
+	# print results
 
-
+	# newJson = json.dumps(jsonStr, indent=4)
+	with open('allresults.json','w') as fileOut:
+			fileOut.write(df.sort_values(by='value',ascending=False).to_json(orient='records'))
+	with open('yearMean.json','w') as fileOut:
+			fileOut.write(yearMean.to_json())
+	with open('yearSum.json','w') as fileOut:
+			fileOut.write(yearSum.to_json())
+	with open('summary.json','w') as fileOut:
+			fileOut.write(json.dumps(results["summary"]))							
+			
+	return results
--- app/timeserious.py
+++ app/timeserious.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from flask import Flask, render_template, json, request,redirect,session,jsonify
-import simplejson as simpljson
+from flask import Flask, render_template,request,redirect,session,jsonify
+import simplejson as json
 import pandas
 import csv
 import analyse
@@ -24,10 +24,10 @@ def addData():
 
         if _url and _source and _units:
 
-        	if _units == 'Tonnes of CO2 equivalent gas':
-        		#blah
-	        	results = analyse.analyseData(_url,_source,_units,_entity)
-	        	return render_template('emissions.html', results=results)
+            if _units == 'tonnes of CO2 equivalent gas':
+                #blah
+                results = analyse.analyseData(_url,_source,_units,_entity)
+                return render_template('emissions.html', results=results,units=_units,source=_source,entity=_entity)
 
     except Exception as e:
         return render_template('error.html',error = str(e))
